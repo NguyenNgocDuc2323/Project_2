bf2f905a3b9592a8b36ded386b5d8fdf32a1dfd5
Ninh <haininh320@gmail.com>
2025-03-17 22:17:19 +0700

    update navigation to redirect to login instead of menu

--- src/main/java/com/example/manage_account/MainApplication.java
+++ src/main/java/com/example/manage_account/MainApplication.java
@@ -21,7 +21,7 @@ public class MainApplication extends Application {
             Locale vietnam = new Locale("vn", "VN");
             Translator.setLocale(vietnam);
             Navigator.getInstance().setState(stage);
-            Navigator.getInstance().gotoMenu();
+            Navigator.getInstance().gotoLogin();
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Lỗi khi load FXML! Kiểm tra đường dẫn.");
