3805467ad5dfb3efe78085faf41350ef33d97e06
NguyenNgocDuc32323 <nguyenngocduc332@gmail.com>
2025-04-12 17:54:43 +0700

    fix error

--- src/main/java/model/OrderDetail.java
+++ src/main/java/model/OrderDetail.java
@@ -9,6 +9,7 @@ public class OrderDetail {
     private Double unitPrice;
     private String categoryName;
 
+    public OrderDetail() {}
     public OrderDetail(int id, int orderId, int productId,
                        String productName, String categoryName,
                        int quantity, double unitPrice) {
